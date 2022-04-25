2bd3cff697de2871317c8c61c0b05e0bfb8b24ab
Nguyen Xuan Hung <32128100+xuanhungptithcm@users.noreply.github.com>
2022-04-26 02:00:11 +0700

    Features/create 404 page (#6)
    * Develop (#5)
    
    * update unit test
    
    * remove new line
    
    Co-authored-by: Hung Nguyen Xuan <xuanhungptithcm@gmail.com>
    
    * update readme.md
    
    * create 404 page and update test
    
    Co-authored-by: Barry <barry.nguyen_fp@ntucguest.com>

--- src/api/__tests__/hooks.test.ts
+++ src/api/__tests__/hooks.test.ts
@@ -1,21 +1,131 @@
+/**
+ * @jest-environment jsdom
+ */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable jest/no-mocks-import */
-import { useContext } from "react";
-import { useHistory } from "react-router-dom";
+
 import { useGeneralContext } from "../../components/context/hooks";
 import { useAxiosGet } from "../../hooks/useAxiosGet";
-import { useLogin } from '../hooks';
-import { mockUseCallback, mockUseContext, mockUseEffect, mockUseState } from './../../__mocks__/GenericReactMocks';
+import { useInitUserInfo, useLogin, useStoreUserInfo } from "../hooks";
+import {
+  mockUseCallback,
+  mockUseContext,
+  mockUseEffect,
+  mockUseState,
+} from "./../../__mocks__/GenericReactMocks";
 
 jest.mock("../../components/context/constants");
 
+describe("useStoreUserInfo", () => {
+  const sessionStorageMock = (() => {
+    let store: any = {};
+    return {
+      getItem(key: any) {
+        return store[key] || null;
+      },
+      setItem(key: any, value: any) {
+        store[key] = value.toString();
+      },
+      removeItem(key: any) {
+        delete store[key];
+      },
+      clear() {
+        store = {};
+      },
+    };
+  })();
+  Object.defineProperty(window, "sessionStorage", {
+    value: sessionStorageMock,
+  });
+  test("should set user info in session storage", () => {
+    mockUseCallback();
+    const setStore = useStoreUserInfo();
+    const setItemSpy = jest.spyOn(window.sessionStorage, "setItem");
+    setStore({ username: "123" } as any);
+    expect(sessionStorageMock.setItem).toBeCalled();
+    expect(sessionStorageMock.setItem).toBeCalledWith(
+      "user",
+      JSON.stringify({ username: "123" })
+    );
+  });
+  test("should not set value", () => {
+    jest.resetAllMocks();
+    const setItemSpy = jest.spyOn(window.sessionStorage, "setItem");
+    mockUseCallback();
+    jest.mock("../hooks");
+    const setStore = useStoreUserInfo();
+    setStore("" as any);
+    expect(sessionStorageMock.setItem).toBeCalledTimes(0);
+  });
+});
 
+describe("useInitUserInfo", () => {
+  const sessionStorageMock = (() => {
+    let store: any = {};
+    return {
+      getItem(key: any) {
+        return store[key] || null;
+      },
+      setItem(key: any, value: any) {
+        store[key] = value.toString();
+      },
+      removeItem(key: any) {
+        delete store[key];
+      },
+      clear() {
+        store = {};
+      },
+    };
+  })();
+  Object.defineProperty(window, "sessionStorage", {
+    value: sessionStorageMock,
+  });
+  beforeEach(() => {
+    mockUseEffect();
+  });
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  test("should init user info", () => {
+    window.sessionStorage.setItem("user", JSON.stringify({ username: "123" }));
+    const getItem = jest.spyOn(window.sessionStorage, "getItem");
+    let setter: any = jest.fn();
+    (useGeneralContext as any) = () => ({
+      setGeneralContext: jest.fn((fn) => (setter = fn)),
+    });
+    useInitUserInfo();
+    expect(sessionStorage.getItem).toBeCalled();
+    expect(
+      setter({
+        other: 123,
+      })
+    ).toMatchObject({
+      isLoggedIn: true,
+      other: 123,
+      userInfo: {
+        username: "123",
+      },
+    });
+  });
+  test("should not init user info", () => {
+    window.sessionStorage.setItem("user", "");
+    const getItem = jest.spyOn(window.sessionStorage, "getItem");
+    let setter: any = jest.fn();
+    (useGeneralContext as any) = () => ({
+      setGeneralContext: setter,
+    });
+    useInitUserInfo();
+    expect(sessionStorage.getItem).toBeCalled();
+    expect(setter).toBeCalledTimes(0);
+  });
+});
 describe("useLogin", () => {
   let contextSetter: any;
+  const setUserInfo: any = jest.fn();
   const setKey = jest.fn();
   const userLogin = {
-    email: 'abc@gmail.com',
-    password: '123456'
+    email: "abc@gmail.com",
+    password: "123456",
   };
 
   beforeEach(() => {
@@ -24,8 +134,9 @@ describe("useLogin", () => {
     mockUseContext();
     mockUseEffect();
     (useGeneralContext as any) = () => ({
-      setGeneralContext: jest.fn((fn) => contextSetter = fn)
+      setGeneralContext: jest.fn((fn) => (contextSetter = fn)),
     });
+    (useStoreUserInfo as any) = () => setUserInfo;
   });
   afterEach(() => {
     jest.resetAllMocks();
@@ -36,12 +147,12 @@ describe("useLogin", () => {
 
     (useAxiosGet as any) = jest.fn().mockImplementationOnce(() => ({
       response: {
-        "createdAt": "2021-10-21T08:36:53.248Z",
-        "name": "Jimmie Thompson",
-        "avatar": "https://cdn.fakercloud.com/avatars/sergeysafonov_128.jpg",
-        "username": "Mona_Kassulke14",
-        "password": "EECsjlVnWIXfeuA",
-        "id": "1"
+        createdAt: "2021-10-21T08:36:53.248Z",
+        name: "Jimmie Thompson",
+        avatar: "https://cdn.fakercloud.com/avatars/sergeysafonov_128.jpg",
+        username: "Mona_Kassulke14",
+        password: "EECsjlVnWIXfeuA",
+        id: "1",
       },
       getError: null,
     }));
@@ -51,15 +162,24 @@ describe("useLogin", () => {
     login(userLogin);
     expect(setKey).toBeCalledTimes(1);
     expect(contextSetter()).toMatchObject({
-      "isLoggedIn": true,
-      "response": {
-        "avatar": "https://cdn.fakercloud.com/avatars/sergeysafonov_128.jpg",
-        "createdAt": "2021-10-21T08:36:53.248Z",
-        "id": "1",
-        "name": "Jimmie Thompson",
-        "password": "EECsjlVnWIXfeuA",
-        "username": "Mona_Kassulke14",
+      isLoggedIn: true,
+      userInfo: {
+        avatar: "https://cdn.fakercloud.com/avatars/sergeysafonov_128.jpg",
+        createdAt: "2021-10-21T08:36:53.248Z",
+        id: "1",
+        name: "Jimmie Thompson",
+        password: "EECsjlVnWIXfeuA",
+        username: "Mona_Kassulke14",
       },
+    });
+    expect(setUserInfo).toBeCalled();
+    expect(setUserInfo).toBeCalledWith({
+      avatar: "https://cdn.fakercloud.com/avatars/sergeysafonov_128.jpg",
+      createdAt: "2021-10-21T08:36:53.248Z",
+      id: "1",
+      name: "Jimmie Thompson",
+      password: "EECsjlVnWIXfeuA",
+      username: "Mona_Kassulke14",
     });
   });
 
@@ -73,5 +193,4 @@ describe("useLogin", () => {
     login(userLogin);
     expect(contextSetter).not.toBeCalled();
   });
-
 });
