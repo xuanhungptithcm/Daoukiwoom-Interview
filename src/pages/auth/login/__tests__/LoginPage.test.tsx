6f0dfe5ca42545d5b49dd3b713151d34cce35084
Nguyen Xuan Hung <32128100+xuanhungptithcm@users.noreply.github.com>
2022-04-27 09:25:00 +0700

    Bugs/fix regex and update route (#13)
    * fix regex password not correct, fix route
    
    * update unit test
    
    * test submit form
    
    Co-authored-by: Hung Nguyen Xuan <hung.nguyenxuan@petronas.com>

--- src/pages/auth/login/__tests__/LoginPage.test.tsx
+++ src/pages/auth/login/__tests__/LoginPage.test.tsx
@@ -2,12 +2,47 @@
  * @jest-environment jsdom
  */
 
-import { shallow } from "enzyme";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useLogin } from "../../../../api/hooks";
 import LoginPage from "../LoginPage";
 
+interface IInputType extends HTMLElement {
+  value: string;
+}
 describe("LoginPage", () => {
-  test("should render correct", () => {
-    expect(shallow(<LoginPage />)).toMatchSnapshot();
-  });
+  const setup = () => {
+    const utils = render(
+      <Router>
+        <LoginPage />
+      </Router>
+    );
+    const emailInput = screen.getByLabelText<IInputType>("email-input");
+    const emailPassword = screen.getByLabelText<IInputType>("password-input");
+    const loginForm = screen.getByLabelText<any>("login-form");
 
+    return {
+      ...utils,
+      emailInput,
+      emailPassword,
+      loginForm,
+    };
+  };
+
+  test("should render correct", () => {
+    const login = jest.fn();
+    const handleSubmit = jest.fn();
+    (useLogin as any) = () => login;
+
+    const { emailInput, emailPassword, loginForm } = setup();
+    fireEvent.change(emailInput, { target: { value: "xuanhung@gmail.com" } });
+    expect(emailInput.value).toBe("xuanhung@gmail.com");
+    fireEvent.change(emailPassword, { target: { value: "Abc1234456_" } });
+    expect(emailPassword.value).toBe("Abc1234456_");
+
+    loginForm.onsubmit = handleSubmit;
+    fireEvent.submit(loginForm);
+    expect(handleSubmit).toBeCalledTimes(1);
+  });
 });
