1f145fe5c1b194b71d66e989659375954318f759
Hung Nguyen Xuan <hung.nguyenxuan@petronas.com>
2022-04-27 08:51:13 +0700

    update unit test

--- src/pages/auth/login/__tests__/LoginPage.test.tsx
+++ src/pages/auth/login/__tests__/LoginPage.test.tsx
@@ -2,12 +2,49 @@
  * @jest-environment jsdom
  */
 
-import { shallow } from "enzyme";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useLogin } from "../../../../api/hooks";
 import LoginPage from "../LoginPage";
 
+interface IInputType extends HTMLElement {
+  value: string;
+}
 describe("LoginPage", () => {
-  test("should render correct", () => {
-    expect(shallow(<LoginPage />)).toMatchSnapshot();
-  });
+  const setup = () => {
+    const utils = render(
+      <Router>
+        <LoginPage />
+      </Router>
+    );
+    const emailInput = screen.getByLabelText<IInputType>("email-input");
+    const emailPassword = screen.getByLabelText<IInputType>("password-input");
+    const loginForm = screen.getByLabelText<IInputType>("login-form");
 
+    return {
+      ...utils,
+      emailInput,
+      emailPassword,
+      loginForm,
+    };
+  };
+
+  test("should render correct", () => {
+    const login = jest.fn();
+    (useLogin as any) = () => login;
+
+    const { emailInput, emailPassword, loginForm } = setup();
+    console.log(emailInput);
+    fireEvent.change(emailInput, { target: { value: "user01" } });
+    expect(emailInput.value).toBe("user01");
+    fireEvent.change(emailPassword, { target: { value: "123456789" } });
+    expect(emailPassword.value).toBe("123456789");
+
+    fireEvent.submit(loginForm, { target: {
+      email: { value: "xuanhung@gmail.com" },
+      password: { value: "Abc123123123_" },
+    } });
+    expect(login).toBeCalledTimes(0);
+  });
 });
